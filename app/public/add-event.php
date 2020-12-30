071bae7d2ae7500cdd46321782c8f1360421d5f5
Vincent Laureys <vincent.laureys@student.odisee.be>
2020-12-30 17:46:11 +0100

    tickets en evenementen toevoegpagina

---
+++ app/public/add-event.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../resources/templates');
+$twig = new \Twig\Environment($loader);
+$router = new \Bramus\Router\Router();
+
+// General variables
+$basePath = __DIR__ . '/../';
+
+require_once $basePath . 'vendor/autoload.php';
+require_once $basePath . 'src/Models/Events.php';
+require_once $basePath . 'src/functions.php';
+
+
+
+
+echo $twig->render('pages/add-event.twig', [
+
+]);
