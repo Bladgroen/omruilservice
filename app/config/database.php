ab437e8b9f30aa63c43ff2fa413e0354eb7122e9
Vincent Laureys <vincent.laureys@student.odisee.be>
2021-05-24 14:31:25 +0200

    change database

--- app/config/database.php
+++ app/config/database.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_HOST', 'mysqldb');
-define('DB_USER', 'root');
-define('DB_PASS', 'Azerty123');
-define('DB_NAME_FF', 'omruilservice');
+define('DB_HOST', '10.129.28.186');
+define('DB_USER', 'vincent2');
+define('DB_PASS', 'Brbw69!6');
+define('DB_NAME_FF', 'omruilservice.sql');
 define('DB_NAME_STATUS', 'status');
 
